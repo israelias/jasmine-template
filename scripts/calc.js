8934a2e8342f6e0a7dec550be6c8ed47b0b9e45f
israelias <israelias.js@gmail.com>
2020-12-03 01:35:05 +0800

    AppRefactor scripts for defensively programmed specifications

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,26 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
+function Calculator() {
+    this.value = 0;
+}
+
+// Everytime our add function is called
+Calculator.prototype.add = function(number) {
+    // if it's a number
+    if (typeof (number) == "number") {
+        // increment our running total
+        this.value += number;
+        // otherwise
     } else {
-        return "Error!";
+        // return an error
+        alert("Error!");
     }
 }
+
+// non-refactored version
+
+// function addition(numOne, numTwo) {
+//     if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
+//         return numOne + numTwo;
+//     } else {
+//         alert("Error!");
+//     }
+// }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,17 +1,30 @@
 // I want to test a calculator
 describe("Calculator", function() {
+
+    // before each function, create a new instance of Calculator
+    beforeEach(function() {
+        calc = new Calculator();
+    });
+
     // I am going to test the addition function
     describe("Addition tests", function() {
         // I want to get the result of 42
         it("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
             // I expect the result of 20 + 22 to be 42
-            expect(addition(20, 22)).toBe(42);
+            expect(calc.value).toBe(42);
         })
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         })
         it("should return an error if we don't supply two numbers", function() {
-            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+            // spy on the window object's alert function
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         })
     })
 })
